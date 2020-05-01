0914fd175a5a531a48f4eea0bef346cc5fbfce1c
kimani kelly <kimanikelly@yahoo.com>
2020-05-01 09:21:40 -0400

    The apiCredentials.js file was imported into the server.js file allowing access to the .env file

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+// Loads the path module
+// path module used to read directories and file paths
 var path = require('path');
+
+// Loads environment variables from the .env file into process.env
+// Configures the dotenv module
+require('dotenv').config();
+
+var apiCredentials = require('./apiCredentials');
+
+console.log(apiCredentials);
