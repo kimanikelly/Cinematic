28ede67f284c90020461a5db59635192827a8265
kimani kelly <kimanikelly@yahoo.com>
2020-05-01 13:54:04 -0400

    Deleted Axios CDN and the token route works on the front end

--- app/public/assets/javascript/logic.js
+++ app/public/assets/javascript/logic.js
@@ -1,4 +1,7 @@
+
 // Detects the state of the state of the DOM before JavaScript code is executed
 $(document).ready(function () {
-
+    $.post('/token', function (res) {
+        console.log(res);
+    })
 })
