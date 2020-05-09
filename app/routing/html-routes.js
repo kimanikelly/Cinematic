ea2607b41948b98a06073074dc132d2fcad6aeda
kimani kelly <kimanikelly@yahoo.com>
2020-05-08 22:38:37 -0400

    Changed the directory grid to bootstrap nav

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -7,6 +7,7 @@ module.exports = function (app) {
     // GET route used to render index.html
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'))
+
     });
 
     // GET route used to render shows.html
